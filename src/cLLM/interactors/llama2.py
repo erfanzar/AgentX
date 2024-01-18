a774601ac67a40fb8672e14a55ac29c3254c04a0
Erfan Zare Chavoshi <59269023+erfanzar@users.noreply.github.com>
2024-01-18 04:17:31 -0800

    Create llama2.py

---
+++ src/cLLM/interactors/llama2.py
@@ -0,0 +1,49 @@
+from ._base import BaseInteract
+from typing import List, Optional
+
+class Llama2Interact(BaseInteract):
+    def __init__(
+            self,
+            user_name: str,
+            assistant_name: str,
+    ):
+        user_prefix = "<s>[INST]"
+        assistant_prefix = "[/INST]"
+        super().__init__(
+            user_name=user_name,
+            user_message_token=user_prefix,
+            assistant_name=assistant_name,
+            assistant_message_token=assistant_prefix,
+            prompter_type="llama",
+            end_of_turn_token="</s>",
+        )
+
+    def format_history_prefix(
+            self,
+            history: list[list[str]],
+            system_message: str,
+    ):
+        prompt = ""
+        for user, assistant in history:
+            prompt += f"{self.user_message_token}{user} "
+            prompt += f"{self.assistant_message_token}{assistant} "
+
+        return prompt
+
+    def format_message(
+            self,
+            prompt: str,
+            history: list[list[str]],
+            system_message: Optional[str],
+            prefix: Optional[str]
+    ) -> str:
+
+        dialogs = prefix if prefix is not None else ""
+
+        for user, assistant in history:
+            dialogs += f"{self.user_message_token}{user}"
+            dialogs += f"{self.assistant_message_token}{assistant}"
+
+        dialogs += f"{self.user_message_token}{prompt}"
+        dialogs += self.assistant_message_token
+        return dialogs
