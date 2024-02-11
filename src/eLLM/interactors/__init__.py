f037ae6c5dbd527abd8286a22528435a117d65e9
Erfan Zare Chavoshi <59269023+erfanzar@users.noreply.github.com>
2024-02-11 02:19:04 -0800

    Update __init__.py

--- src/eLLM/interactors/__init__.py
+++ src/eLLM/interactors/__init__.py
@@ -3,4 +3,4 @@ from .cargo import CargoInteract
 from .guanaco import GuanacoInteract
 from .openchat import OpenChatInteract
 from .llama2 import Llama2Interact
-from .qwen2 import Qwen2Interact
+from .chatml import ChatMLInteract
