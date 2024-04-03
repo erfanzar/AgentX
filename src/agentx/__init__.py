8e98728c0c61dd68def50591dd7b5effe524411a
erfanzar <erfanzare82@yahoo.com>
2024-04-03 14:17:54 +0330

    Starting `ollama` server automaticaly if its available

--- src/agentx/__init__.py
+++ src/agentx/__init__.py
@@ -1,3 +1,12 @@
+try:
+    import ollama as _ollama
+    import threading
+    import os
+
+    threading.Thread(target=os.system, args=("ollama serve",)).start()
+except ModuleNotFoundError:
+    ...
+
 from .prompt_templates import PromptTemplates
 from .agents import (
     ChatAgent,
