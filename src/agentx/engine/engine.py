fe06ec1df4b6536727c586e01a991253a714abd7
erfanzar <erfanzare82@yahoo.com>
2024-04-23 17:18:53 +0330

    removing useless parts of engine (padding for torch)

--- src/agentx/engine/engine.py
+++ src/agentx/engine/engine.py
@@ -103,7 +103,6 @@ class ServeEngine:
             add_special_tokens=False,
             truncation=True,
             max_length=max_sequence_length - (max_new_tokens // 2),
-            padding="max_length"
         ).to(self.model.device)
         inputs = dict(
             **in_ids,
@@ -146,7 +145,6 @@ class ServeEngine:
                 add_special_tokens=False,
                 max_length=max_sequence_length,
                 truncation=True,
-                padding="max_length"
             ).to(self.model.device),
             generation_config=GenerationConfig(
                 top_k=top_k,
